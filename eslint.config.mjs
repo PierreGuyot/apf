5b9eaacab998eb57673dcd36c600973b7c1be0b4
Pierre Guyot <pierre@hopserve.io>
2024-04-21 17:48:28 +0200

    Add eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,76 @@
+import eslint from "@eslint/js";
+import pluginImportOrder from "eslint-plugin-import";
+import pluginArrowFunctions from "eslint-plugin-prefer-arrow-functions";
+import pluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import pluginReactRecommended from "eslint-plugin-react/configs/recommended.js";
+
+import globals from "globals";
+
+import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
+
+const config = [
+  {
+    ignores: [
+      "dist/**/*",
+      "node_modules/**/*",
+      ".yarn/**/*",
+      ".pnp.*",
+      "*.config.*js",
+    ],
+  },
+  { languageOptions: { globals: globals.browser } },
+  ...compat.extends("standard-with-typescript"),
+  eslint.configs.recommended,
+  pluginReactRecommended,
+  pluginPrettierRecommended,
+  {
+    plugins: {
+      "plugin-arrow-functions": pluginArrowFunctions,
+      "plugin-import-order": pluginImportOrder,
+    },
+    rules: {
+      // De-activated rules
+      "no-console": "off",
+      "no-extra-boolean-cast": "off",
+      "no-useless-constructor": "off",
+      "unicorn/prefer-includes": "off",
+
+      // Added rules
+      "prefer-template": "error",
+      "prefer-arrow-callback": "error",
+      "plugin-arrow-functions/prefer-arrow-functions": "error",
+      "sort-imports": "warn",
+      "plugin-import-order/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "internal",
+            "external",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+          ],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+          },
+        },
+      ],
+    },
+  },
+];
+
+export default config;
