a808839aa471b85837a379330e224a69775f8c16
Pierre Guyot <pierre@hopserve.io>
2024-05-08 12:43:20 +0200

    Linter fixes

--- src/pages/BiopsiesProstatiques/BiopsiesProstatiques.tsx
+++ src/pages/BiopsiesProstatiques/BiopsiesProstatiques.tsx
@@ -36,14 +36,14 @@ const getScore = (rows: Row[]): Score => {
   const tumorCount = sum(rows.map((row) => row.tumorCount));
   const tumorScore = tumorCount
     ? {
-      tumorSize: sumArrays(rowsWithTumor.map((row) => row.tumorSize)),
-      tumorGleason: getMaximumByGleasonScore(
-        rowsWithTumor.map((row) => row.tumorGleason),
-      ),
-      tumorEpn: rowsWithTumor.map((row) => row.tumorEpn).some(Boolean),
-      tumorTep: rowsWithTumor.map((row) => row.tumorTep).some(Boolean),
-      tumorPin: rowsWithTumor.map((row) => row.tumorPin).some(Boolean),
-    }
+        tumorSize: sumArrays(rowsWithTumor.map((row) => row.tumorSize)),
+        tumorGleason: getMaximumByGleasonScore(
+          rowsWithTumor.map((row) => row.tumorGleason),
+        ),
+        tumorEpn: rowsWithTumor.map((row) => row.tumorEpn).some(Boolean),
+        tumorTep: rowsWithTumor.map((row) => row.tumorTep).some(Boolean),
+        tumorPin: rowsWithTumor.map((row) => row.tumorPin).some(Boolean),
+      }
     : {};
 
   return {
