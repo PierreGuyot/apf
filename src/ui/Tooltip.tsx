201bf914912d4c26f5c66b0e8f1a2a0d1a509c63
Pierre Guyot <pierre@hopserve.io>
2024-05-28 16:32:02 +0200

    Rework tooltip component

--- src/ui/Tooltip.tsx
+++ src/ui/Tooltip.tsx
@@ -1,30 +1,60 @@
 import { useBoolean } from "./helpers/state";
 import "./tooltip.css";
 
-import { PropsWithChildren, ReactNode } from "react";
+import { PropsWithChildren, ReactNode, useEffect, useRef } from "react";
 
-type Props = PropsWithChildren<{ content: ReactNode }>;
+type Props = PropsWithChildren<{
+  mode?: "click" | "hover";
+  content: ReactNode;
+}>;
 
-export const Tooltip = ({ content, children }: Props) => {
-  const [isHovered, setIsHovered] = useBoolean(false);
+export const Tooltip = ({ mode = "hover", content, children }: Props) => {
+  const [isOpen, setIsOpen] = useBoolean(false);
+  const open = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
 
-  const onMouseEnter = () => {
-    setIsHovered(true);
+  const tooltipContent = useRef<HTMLDivElement | null>(null);
+  const tooltipHandle = useRef<HTMLDivElement | null>(null);
+
+  // Clear popover on click-outside
+  const onMouseDown = (event: MouseEvent) => {
+    // CAUTION: this cast is type-unsafe
+    const targetNode = event.target as Node | null;
+
+    if (
+      tooltipContent.current &&
+      !tooltipContent.current.contains(targetNode)
+    ) {
+      close();
+    }
   };
-  const onMouseLeave = () => {
-    setIsHovered(false);
-  };
+
+  const callbacks =
+    mode === "hover"
+      ? { onMouseEnter: open, onMouseLeave: close }
+      : { onClick: open };
+
+  useEffect(() => {
+    if (mode === "hover") {
+      window.addEventListener("scroll", close, true);
+      return () => window.removeEventListener("scroll", close, true);
+    } else {
+      // Handle clicking outside of the tooltip
+      document.addEventListener("mousedown", onMouseDown);
+      return () => document.removeEventListener("mousedown", onMouseDown);
+    }
+  }, [mode]);
 
   return (
     <span className="tooltip">
-      <span
-        className="tooltip-handle"
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-      >
+      <span className="tooltip-handle" ref={tooltipHandle} {...callbacks}>
         <span>{children}</span>
       </span>
-      {isHovered ? <div className="tooltip-content">{content}</div> : undefined}
+      {isOpen ? (
+        <div className="tooltip-content" ref={tooltipContent}>
+          {content}
+        </div>
+      ) : undefined}
     </span>
   );
 };
