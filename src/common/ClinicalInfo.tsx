375c79dbe8fb77322ee2093fcafa6259229a794d
Pierre Guyot <pierre@hopserve.io>
2024-06-14 15:52:39 +0200

    Clean ClinicalInfo

--- src/common/ClinicalInfo.tsx
+++ src/common/ClinicalInfo.tsx
@@ -1,6 +1,5 @@
 import { InputTextArea } from "../ui/InputTextArea";
 import { Section } from "../ui/Section";
-import { Title } from "../ui/Title";
 import { FieldProps } from "../ui/helpers/fields";
 
 type Props = FieldProps<string> & {
@@ -8,8 +7,7 @@ type Props = FieldProps<string> & {
 };
 
 export const ClinicalInfo = ({ index, value, onChange }: Props) => (
-  <Section>
-    <Title title="Renseignements cliniques" index={index} />
+  <Section title="Renseignements cliniques" index={index}>
     <InputTextArea
       value={value}
       placeholder="Ajoutez ici les renseignements cliniques"
