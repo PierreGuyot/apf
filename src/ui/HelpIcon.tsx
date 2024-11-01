e3469d6abf2a686d2dc1feccf04cd6d3b4d0a5f8
Pierre Guyot <pierre@hopserve.io>
2024-11-01 20:06:56 +0100

    Update HelpIcon

--- src/ui/HelpIcon.tsx
+++ src/ui/HelpIcon.tsx
@@ -1,13 +1,19 @@
 import { ReactNode } from "react";
 import { Tooltip } from "./Tooltip";
 import css from "./help-icon.module.css";
+import { join } from "./helpers";
+import { Text } from "./Text";
 
+// TODO CLEAN: add size variants on design page
 type Props = {
+  size?: "sm" | "md";
   content: ReactNode;
 };
 
-export const HelpIcon = ({ content }: Props) => (
-  <Tooltip content={content}>
-    <div className={css.main}>?</div>
+export const HelpIcon = ({ size = "md", content }: Props) => (
+  <Tooltip content={<div className={css.content}>{content}</div>}>
+    <div className={join(css.main, css[size])}>
+      <Text size={size}>?</Text>
+    </div>
   </Tooltip>
 );
