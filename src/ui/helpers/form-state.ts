54e51a7c0fe25f514f29c4997d639146027ecd0c
Pierre Guyot <pierre@hopserve.io>
2024-11-01 20:06:44 +0100

    Add TODO comments

--- src/ui/helpers/form-state.ts
+++ src/ui/helpers/form-state.ts
@@ -16,6 +16,7 @@ import { useCallback, useMemo, useState } from "react";
  * This might seem to be a bit complex at first, but it's actually very straightforward, simple sugar.
  * It allows for way cleaner form syntax, especially when composing them.
  */
+// TODO CLEAN: consider renaming
 export const patchState =
   <State>(state: State, setState: (value: State) => void): SetState<State> =>
   (key) =>
@@ -26,7 +27,7 @@ type SetState<State> = <K extends keyof State>(
   key: K,
 ) => (value: State[K]) => void;
 
-// TODO clean: consider creating a `useValue` helper ({value: T, errors: string[], set: (value: T) => void })
+// TODO CLEAN: consider creating a `useValue` helper ({value: T, errors: string[], set: (value: T) => void })
 // to facilitate adding in-line validations
 // This would mean passing a validation function to useForm
 
